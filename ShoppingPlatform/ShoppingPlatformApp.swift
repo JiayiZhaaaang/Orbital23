bf65d4636a8bf6d7fcb1c259d6318c720bb7ea77
JiayiZhaaaang <dywangcheng12345@163.com>
2023-05-28 18:21:03 +0800

    Initial Commit

---
+++ ShoppingPlatform/ShoppingPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingPlatformApp.swift
+//  ShoppingPlatform
+//
+//  Created by wl on 2023/5/28.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
